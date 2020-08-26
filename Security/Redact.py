9d0e5b4553aec8487ab2ae2e2f1f7562a42b8f36
Sam Jenkins <satalketo@gmail.com>
2020-08-26 07:22:24 +0100

    Started adding argparse

--- Security/Redact.py
+++ Security/Redact.py
@@ -1,9 +1,27 @@
 import re
+import argparse
 
 # Constant
-# We could use a much better regex that wouldn't handle 999.999.999.999
+# We could use a much better regex that wouldn't match 999.999.999.999
 # but let's go with this one for the moment.
 IPADDR_REGEX = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+
+# Get the arguments that have been passed to the script
+#https://stackabuse.com/command-line-arguments-in-python/
+parser = argparse.ArgumentParser()
+parser.add_argument("--version", "-v", help="Show program version", action="store_true")
+parser.add_argument("--file", "-f", help="Run against a single file", action="store_true")
+parser.add_argument("--dir", "-d", help="Run against a whole directory", action="store_true")
+parser.add_argument("--help", "-h", help="Show this help", action="store_true")
+parser.add_argument("--out", "-o", help="Output redactions to file", action="store_true")
+
+args = parser.parse_args()
+
+if args.version:
+    print("This is myprogram version 0.1")
+if args.help:
+    print("This is where the help would go")
+
 
 def replaceAllIPAddr(fileLocation, ipAddrDict):
     # Create Regex
@@ -27,11 +45,12 @@ def replaceAllIPAddr(fileLocation, ipAddrDict):
 
     return ipAddrDict
 
+
 ipAddrDict = dict()
 fileLocation = r"C:\Users\satal\Downloads\test.txt"
 
-ipAddrDict = replaceAllIPAddr(fileLocation, ipAddrDict)
+#ipAddrDict = replaceAllIPAddr(fileLocation, ipAddrDict)
 
-for keys,values in ipAddrDict.items():
-    print(keys)
-    print(values)
+#for keys,values in ipAddrDict.items():
+#    print(keys)
+#    print(values)
