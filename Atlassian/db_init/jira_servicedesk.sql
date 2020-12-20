0adfc6f17cd3b9941585e09312b178139cb506d9
Sam Jenkins <satalketo@gmail.com>
2020-12-20 13:37:23 +0000

    Granted privileges to notroot

--- Atlassian/db_init/jira_servicedesk.sql
+++ Atlassian/db_init/jira_servicedesk.sql
@@ -1,1 +1,2 @@
 CREATE DATABASE jira_servicedesk WITH OWNER notroot ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;
+GRANT ALL PRIVILEGES ON DATABASE "jira_servicedesk" to notroot;
