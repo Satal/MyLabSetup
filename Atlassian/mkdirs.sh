0fd663a87ad87c1c9d3ea2bdd1e0d0a09dfc3cda
Sam Jenkins <satalketo@gmail.com>
2020-12-23 13:25:31 +0000

    Added mkdirs.sh as I keep having to do it

---
+++ Atlassian/mkdirs.sh
@@ -0,0 +1,8 @@
+mkdir -p /var/atlassian/bitbucket-data
+mkdir -p /var/atlassian/confluence-data
+mkdir -p /var/atlassian/jira-data
+mkdir -p /var/atlassian/servicedesk-data
+mkdir -p /var/postgres-data
+
+chmod 777 /var/atlassian -R
+chmod 777 /var/postgres-data -R
