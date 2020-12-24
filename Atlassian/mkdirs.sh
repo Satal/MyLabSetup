5a3ffe393d6addc2dc96af116e7afa6c0ab44219
Sam Jenkins <satalketo@gmail.com>
2020-12-24 11:46:19 +0000

    Added restart to nginx and permission change

--- Atlassian/mkdirs.sh
+++ Atlassian/mkdirs.sh
@@ -5,4 +5,6 @@ mkdir -p /var/atlassian/servicedesk-data
 mkdir -p /var/postgres-data
 
 chmod 777 /var/atlassian -R
+chmod g+s /var/atlassian
 chmod 777 /var/postgres-data -R
+chmod g+s /var/postgres-data
